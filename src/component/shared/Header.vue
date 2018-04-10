85e1191b18cef369976a8ebf9d184769ad5712d6
leonardo a <leonardo.akato@gmail.com>
2018-04-10 17:40:16 -0300

    added minor typo fix

---
+++ src/component/shared/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="apiheader">
+    <h2>Cielo - Informações estratégicas</h2>
+    <h1>Demandas</h1>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: 'apiheader',
+    components:{
+      Header
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
