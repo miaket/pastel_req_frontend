b0232169aab34b4a40de5c559437e86c95aecd8a
leonardo a <leonardo.akato@gmail.com>
2018-05-02 17:38:14 -0300

    creating add request post method - not working

--- src/components/Shared/Formbox.vue
+++ src/components/Shared/Formbox.vue
@@ -5,76 +5,30 @@
         <h1>File a Complaint</h1>
         <hr>
         <app-anlform></app-anlform>
-
       </div>
     </div>
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-        <label for="message">Message</label><br>
+        <label for="message">Describe Request</label><br>
         <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
         <textarea
-          id="message"
+          id="reqmsg"
           rows="5"
           class="form-control"></textarea>
       </div>
     </div>
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <div class="form-group">
-        <label for="sendmail">
-          <input
-            type="checkbox"
-            id="sendmail"
-            value="SendMail"> Send Mail
-        </label>
-        <label for="sendInfomail">
-          <input
-            type="checkbox"
-            id="sendInfomail"
-            value="SendInfoMail"> Send Infomail
-        </label>
-        </div>
-
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
-        <label for="male">
-        <input
-            type="radio"
-            id="male"
-            value="Male"> Male
-        </label>
-        <label for="female">
-        <input
-            type="radio"
-            id="female"
-            value="Female"> Female
-        </label>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
-        <label for="priority">Priority</label>
-        <select
-          id="priority"
-          class="form-control">
-        <option></option>
-        </select>
-      </div>
-    </div>
-    <hr>
-    <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-        <button
+        <button @click.prevent='submitted'
           class="btn btn-primary">Submit!
         </button>
       </div>
     </div>
-    </form>
+  </form>
 </template>
 
 <script>
+
   import Anlform from '../form/Anlform.vue';
   export default {
     data (){
@@ -84,6 +38,19 @@
     },
     components: {
       appAnlform: Anlform,
+    },
+    methods: {
+      submitted() {
+        console.log('inside vue submitted')
+        //alert('oi');
+        let userreq = new FormData();
+        userreq.append('userName','testing from vue');
+        this.$http.post('user/1/reqcreate', userreq).then(function (response){
+          console.log('Success: ', response.date);
+        }, function(response){
+          console.log('Error: ', response.data);
+        });
+      }
     }
   }
 // <script>
