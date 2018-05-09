3137393576ebdebc9aed9d5d5f8ab8fef3c101da
leonardo a <leonardo.akato@gmail.com>
2018-05-09 09:39:44 -0300

    show username at header, closes #2

--- src/components/Shared/Header.vue
+++ src/components/Shared/Header.vue
@@ -3,14 +3,36 @@
     <img src="../../assets/cielologo.png" id="logo" alt ="Logo">
     <div class="col-xs-12 form-group">
       <h1>Informações estratégicas</h1>
+      <p>{{ user.userName }}</p>
       <hr>
       <h2>Demandas</h2>
     </div>
+    <!-- <textarea v-model="flexUser.userName" name="" id="" cols="30" rows="10"></textarea> -->
+    <!-- <button @click.prevent="changeUserName" class="btn">change User</button> -->
   </div>
 </template>
 
 <script>
-
+  export default {
+    data (){
+      return{
+        user:{
+          userName: this.$store.state.userName
+        },
+        flexUser:{
+          userName: ""
+        }
+      }
+    },
+    // methods: {
+    //   changeUserName () {
+    //     console.log(this.$store.state.userName)
+    //     this.$store.commit('changeUserName',{
+    //       userName: this.flexUser.userName
+    //     })
+    //   },
+    // },
+  }
 </script>
 
 <style scoped>
--- src/store/store.js
+++ src/store/store.js
@@ -5,11 +5,15 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
-    count: 0
+    userId: 0,
+    userName: '',
   },
   mutations: {
     changeUserId(state, payload){
       state.userId = payload.userId
+    },
+    changeUserName(state, payload){
+      state.userName = payload.userName
     }
   }
 });
