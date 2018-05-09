46472259f5d18c303ff821d49c12df708447d1c8
leonardo a <leonardo.akato@gmail.com>
2018-05-08 22:53:09 -0300

    fixed external store vuex

--- src/main.js
+++ src/main.js
@@ -2,23 +2,12 @@ import Vue from 'vue'
 import VueResource from 'vue-resource'
 import App from './App.vue'
 import Vuex from 'vuex'
-//import { store } from './store/store';
+import { store } from './store/store';
 
 Vue.use(VueResource);
 Vue.use(Vuex);
 Vue.http.options.root = 'http://127.0.0.1:3100/api/';
 Vue.prototype.$userId = '1';
-
-const store = new Vuex.Store({
-  state: {
-    userId: 0
-  },
-  mutations: {
-  	changeUserId(state, payload){
-      state.userId = payload.userId
-    }
-  }
-})
 
 new Vue({
   el: '#app',
--- src/store/store.js
+++ src/store/store.js
@@ -3,8 +3,13 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex);
 
-const store = new Vuex.Store({
+export const store = new Vuex.Store({
   state: {
     count: 0
+  },
+  mutations: {
+    changeUserId(state, payload){
+      state.userId = payload.userId
+    }
   }
 });
