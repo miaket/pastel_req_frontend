3da501807094e21e65b00f4c66df6274ef7465b3
leonardo a <leonardo.akato@gmail.com>
2018-05-11 16:00:11 -0300

    add not functional login and stricted pages

---
+++ src/components/views/Myreq.vue
@@ -0,0 +1,28 @@
+<template>
+  <form>
+    <div class="row">
+
+    </div>
+  </form>
+</template>
+
+<script>
+  export default {
+    data (){
+      return{
+        userid:{
+          id:''
+        },
+        userrequest: {
+          content: ''
+        }
+      }
+    },
+    methods: {
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/views/Signin.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="login">
+    <h1>Login</h1>
+    <input type="text" name="username" v-model="input.username" placeholder="Username" />
+    <input type="password" name="password" v-model="input.password" placeholder="Password" />
+    <button type="button" v-on:click="signin()">Signin</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Signin',
+    data() {
+      return {
+        input: {
+          username: "",
+          password: ""
+        }
+      }
+    },
+    methods: {
+
+      signin() {
+        if(this.input.username != "" && this.input.password != "") {
+          this.$http.post('signin', this.input)
+            .then(response => {
+              if (User.username)
+              console.log('Success: ', response.date);
+              this.$router.replace({ name: "secure" });
+            }, function(response){
+            console.log('Error: ', response.data);
+          });
+          // if(this.input.username == this.$parent.mockAccount.username && this.input.password == this.$parent.mockAccount.password) {
+          //   this.$emit("authenticated", true);
+          //   this.$router.replace({ name: "secure" });
+          // } else {
+          //   console.log("The username and / or password is incorrect");
+          // }
+        } else {
+          console.log("A username and password must be present");
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #login {
+    width: 500px;
+    border: 1px solid #CCCCCC;
+    background-color: #FFFFFF;
+    margin: auto;
+    margin-top: 200px;
+    padding: 20px;
+  }
+</style>
