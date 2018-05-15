e3cb0c5c2f84cd02b5a1a69136cf5f68c04cabea
leonardo a <leonardo.akato@gmail.com>
2018-05-15 17:41:01 -0300

    remove navigation from header

--- src/components/Shared/Header.vue
+++ src/components/Shared/Header.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="container">
+  <div class="container py-5">
     <img src="../../assets/cielologo.png" id="logo" alt ="Logo">
     <div class="col-xs-12 form-group">
       <h1>Informações estratégicas</h1>
       <p>{{ user.userName }}</p>
-      <ul class="nav nav-pills">
+      <!-- <ul class="nav nav-pills">
         <router-link to="/home" tag="li" active-class="active" exact><a>Home</a></router-link>
         <router-link to="/form" tag="li" active-class="active"><a>User</a></router-link>
-      </ul>
+      </ul> -->
       <hr>
     </div>
   </div>
@@ -30,6 +30,6 @@
 
 <style scoped>
   #logo{
-     width: 20%
+     width: 30%
   }
 </style>
