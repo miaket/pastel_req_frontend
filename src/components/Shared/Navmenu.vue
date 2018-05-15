0b6287433162e638d0713a416e7b77c3951a5557
leonardo a <leonardo.akato@gmail.com>
2018-05-15 17:40:31 -0300

    add Navigation menu content

---
+++ src/components/Shared/Navmenu.vue
@@ -0,0 +1,29 @@
+<template>
+<nav class="navbar fixed-top navbar-light bg-light">
+  <div class="container">
+      <ul class="nav nav-pills">
+        <router-link class="navbar-brand" to="/home" tag="li" active-class="active" exact><a>Home</a></router-link>
+        <router-link class="navbar-brand" to="/form" tag="li" active-class="active"><a>User</a></router-link>
+      </ul>
+      <hr>
+  </div>
+</nav>
+</template>
+
+<script>
+  export default {
+    data (){
+      return{
+        user:{
+          userName: this.$store.state.userName
+        },
+        flexUser:{
+          userName: ""
+        }
+      }
+    },
+  }
+</script>
+
+<style scoped>
+</style>
