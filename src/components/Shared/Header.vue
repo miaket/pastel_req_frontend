4997abe0a776932f0dfe911f5c5ffc0e886536d8
leonardo a <leonardo.akato@gmail.com>
2018-05-16 21:55:12 -0300

    remove unnecessary comment

--- src/components/Shared/Header.vue
+++ src/components/Shared/Header.vue
@@ -4,10 +4,6 @@
     <div class="col-xs-12 form-group">
       <h1>Informações estratégicas</h1>
       <p v-if="getUsername">Hello, {{ getUsername }}!</p>
-      <!-- <ul class="nav nav-pills">
-        <router-link to="/home" tag="li" active-class="active" exact><a>Home</a></router-link>
-        <router-link to="/form" tag="li" active-class="active"><a>User</a></router-link>
-      </ul> -->
       <hr>
     </div>
   </div>
