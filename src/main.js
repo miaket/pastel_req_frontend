f221a3cf025297fa5cedeed06a2fa68cee689fea
leonardo a <leonardo.akato@gmail.com>
2018-06-01 22:50:42 -0300

    add material icons

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import { routes } from './routes';
 import { store } from './store/store';
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify)
 Vue.use(VueRouter);
