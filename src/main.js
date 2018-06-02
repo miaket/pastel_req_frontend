e54c1e85de214673019a7e0d1bf721dad5889540
leonardo a <leonardo.akato@gmail.com>
2018-06-02 01:12:32 -0300

    fix webpack

--- src/main.js
+++ src/main.js
@@ -6,8 +6,6 @@ import VueRouter from 'vue-router';
 import { routes } from './routes';
 import { store } from './store/store';
 import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.use(Vuetify)
 Vue.use(VueRouter);
