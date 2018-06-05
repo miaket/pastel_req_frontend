759df8a9077ed79fbdfdb5b87c0701438a7e4c06
leonardo a <leonardo.akato@gmail.com>
2018-06-04 22:51:47 -0300

    load requests at mounted event

--- src/components/view/Myreq.vue
+++ src/components/view/Myreq.vue
@@ -3,6 +3,16 @@
     <div class="row">
       <button class="btn btn-lg btn-primary btn-block" type="button" v-on:click="getRequests()">trigger requests</button>
     </div>
+    <ul>
+      <li v-for="request in userRequests.body" :key="request.index">
+        {{ request.createdAt}} - {{ request.requestId }} - {{ request.urgencyLevel }} 
+        {{ request.customers}}
+
+      </li>
+    </ul>
+    <div>
+
+    </div>
   </form>
 </template>
 
@@ -17,21 +27,17 @@
         }
       }
     },
-    methods: {
-      getRequests() {
-        // if(this.input.username != "" && this.input.password != "") {
-        this.$http.get('user/myrequests', this.input)
-          .then(response => {
-            this.userRequests = response;
-            console.log (response)
-            console.log (this.userRequests)
-          }, function(response){
-          console.log('Error: ', response.data);
-        });
-        // } else {
-        //   console.log("A username and password must be present");
-        // }
-      }
+    computed: {},
+    methods: {},
+    mounted(){
+      this.$http.get('user/myrequests')
+        .then(response => {
+          this.userRequests = response;
+          console.log (this.userRequests)
+          return response
+        }, function(response){
+        console.log('Error: ', response.data);
+      });
     }
   }
 </script>
