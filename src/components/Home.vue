11cf36e9ea6a84f15a4adcb4d0875c96316960de
leonardo a <leonardo.akato@gmail.com>
2018-06-04 22:51:57 -0300

    minnor changes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,7 +4,8 @@
         <hr>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab asperiores, aut autem, blanditiis consequuntur
             dolore excepturi laborum maiores minima nihil non nulla obcaecati quas quibusdam quod sed suscipit vero
-            voluptatem.</p>
+            voluptatem.
+        </p>
 
     </div>
 </template>
