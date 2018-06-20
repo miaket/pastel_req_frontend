b8fbf75cb20e3efe7cab1bee8172bf78525627e4
leonardo a <leonardo.akato@gmail.com>
2018-06-20 15:18:49 -0300

    english mf do you speak it

--- src/components/form/Anlform.vue
+++ src/components/form/Anlform.vue
@@ -3,11 +3,11 @@
     <div>
       <input type="radio" id="cb_radio" value="Chargeback" v-model="anlType">
       <label for="one">Chargeback</label>
-      <input type="radio" id="fr_radio" value="Fraude" v-model="anlType">
-      <label for="two">Fraude</label>
+      <input type="radio" id="fr_radio" value="Fraud" v-model="anlType">
+      <label for="two">Fraud</label>
     </div>
     <div class="">
-      <label for="message">EC/CNPJ
+      <label for="message">EC number/CNPJ
       <textarea
         id="message"
         rows="5"
