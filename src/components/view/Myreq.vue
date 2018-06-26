9c8e55d3eda5392769a4e6d207aaaac765ca95ed
leonardo a <leonardo.akato@gmail.com>
2018-06-25 21:35:29 -0300

    css: better style and organization for user requests

--- src/components/view/Myreq.vue
+++ src/components/view/Myreq.vue
@@ -1,14 +1,29 @@
 <template>
   <form>
-    <!-- <div class="row">
-      <button class="btn btn-lg btn-primary btn-block" type="button" v-on:click="getRequests()">trigger requests</button>
-    </div> -->
-    <ul>
-      <li v-for="request in userRequests.body" :key="request.index">
-        {{ request.createdAt}} - {{ request.message }} - {{ request.id }} - {{ request.urgencyLevel }}
-        {{ request.customers }}
-      </li>
-    </ul>
+    <table class="table">
+      <thead class="thead-dark">
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Date of creation</th>
+          <th scope="col">Message</th>
+          <th scope="col">Customers</th>
+          <th scope="col">Urgency</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="request in userRequests.body" :key="request.index">
+          <th scope="row">{{ request.id }}</th>
+          <td>{{ request.createdAt}}</td>
+          <td>{{ request.message }}</td>
+          <td>
+            <ul v-for="customer in request.customers" :key="customer.index">
+              <li>{{ customer.regNumber }} - {{ customer.name }}</li>
+            </ul>
+          </td>
+          <td>{{ request.urgencyLevel }}</td>
+        </tr>
+      </tbody>
+    </table>
   </form>
 </template>
 
